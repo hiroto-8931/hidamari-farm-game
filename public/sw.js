f6e1b120dad2c229bac7866850afac47ceb9b122
hiroto-8931 <ryoo1026@outlook.jp>
2026-08-25 01:47:54 +0900

    Add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+const CACHE='hidamari-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/manifest.webmanifest','/icons/icon.svg']))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html')))));
+
